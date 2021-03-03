300d9c697630a9d1e0d2e15534f5072370af5d76
Ryan Hecht <ryanhecht.tse@gmail.com>
2021-03-03 04:59:48 -0500

    ffwd

--- content.js
+++ content.js
@@ -7,9 +7,21 @@
    holes: ""
 }
 
+course:
+{
+    name: "",
 
+}
 
 */
+
+function calculateCourseHandicap(handicapIndex, courseRating, courseSlope, coursePar) {
+    return Math.floor((handicapIndex * (courseSlope/113)) + (courseRating - coursePar))
+}
+
+function calculateScoreDifferential(courseSlope, adjustedGrossScore, courseRating, pccAdjustment) {
+    return (113 / courseSlope) * (adjustedGrossScore - courseRating - pccAdjustment)
+}
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     scores = []
@@ -17,6 +29,10 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
         function (card, index) {
             // Set this card as the active scorecard
             card.click();
+            
+            for (var i=1; i<19; i++) {
+                
+            }
 
             scores.push({
                 course: card.getElementsByClassName("scorecard-title")[0].innerText,
